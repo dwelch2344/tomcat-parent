5c549c8a9bed481fb5f659da6324d121921b2209
David Welch <david@davidwelch.co>
2012-07-17 16:34:22 -0600

    cleanup for env

--- deploy/src/main/java/launch/Main.java
+++ deploy/src/main/java/launch/Main.java
@@ -1,17 +1,23 @@
 package launch;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.Manager;
 import org.apache.catalina.startup.Tomcat;
 
-import com.dawsonsystems.session.MongoManager;
-import com.dawsonsystems.session.MongoSessionTrackerValve;
+import co.ntier.mongo.tomcat.MongoSessionManager;
+import co.ntier.mongo.tomcat.MongoSessionTrackerValve;
+
 import com.mongodb.ServerAddress;
 
 public class Main {
 
+	private static final Logger logger = Logger.getLogger( Main.class.getName() );
+	
+	// see https://github.com/dawsonsystems/Mongo-Tomcat-Sessions
+	
     public static void main(String[] args) throws Exception {
 
         Tomcat tomcat = new Tomcat();
@@ -25,38 +31,64 @@ public class Main {
         // TODO make this project specific
         File webappFile = new File("web/target/tomcat-web");
         if( !webappFile.exists() ){
-        	System.out.println(" Running locally... " );
+        	logger.info(" Running locally... " );
         	webappFile = new File("../web/target/tomcat-web");
         }
         String message = String.format( "Deploying on port %s the following directory: %s", webPort, webappFile.getAbsolutePath() );
-        System.out.println( message );
+        logger.info( message );
         
-        // TODO wire up a Mongo session?
         Context context = tomcat.addWebapp("/", webappFile.getAbsolutePath());
+        
+     // Wire up the custom session manager if MONGO_SESSION_URL is available
         Manager manager = getManager();
         if(manager != null){
         	context.getPipeline().addValve( new MongoSessionTrackerValve() );
         	context.setManager(manager);
-        	System.out.println( "Designated Tomcat Manager set");
+        	logger.info( "Designated Tomcat Manager set");
         }
-        
-        
         
         tomcat.start();
         tomcat.getServer().await();  
     }
     
     private static Manager getManager(){
+    	
     	try{
-	    	// String url = "mongodb://<user>:<password>@<host>:<port>/<database>";
-	    	ServerAddress address = new ServerAddress("host", 33267);
-	    	MongoManager manager = new MongoManager(address, "session-test", "user", "password");
-	    	// manager.setMaxInactiveInterval(10000);
-	    	System.out.println( "Established MongoManager to " + address);
-	    	return manager;
+    		MongoConnectionDetails conn = parseConnection();
+    		if( conn != null ){
+	    		// just an FYI: this throws a MongoException w/ the message "unauthorized" OR "can't find a master" if you have the wrong details here
+		    	ServerAddress address = new ServerAddress(conn.host, conn.port);
+		    	MongoSessionManager manager = new MongoSessionManager(address, conn.database, conn.user, conn.password);
+		    	logger.info( "Established MongoManager to " + address);
+		    	return manager;
+    		}
     	}catch(Exception e){
-    		e.printStackTrace();
+    		logger.warning("Failed establishing Mongo connection");
+    		throw new RuntimeException("Failed loading Mongo connection", e);
     	}
     	return null;
+    }
+    
+    private static MongoConnectionDetails parseConnection(){
+    	
+    	String url = System.getenv("MONGO_SESSION_URL");
+    	if( url != null){
+	    	MongoConnectionDetails details = new MongoConnectionDetails();
+			String[] parts = url.split(":");
+			details.host = parts[0];
+			details.port = parts.length > 1 ? Integer.valueOf(parts[1]) : 27017;
+			details.database = System.getenv("MONGO_SESSION_DB");
+			details.user = System.getenv("MONGO_SESSION_USER");
+			details.password = System.getenv("MONGO_SESSION_PASS");
+			
+	    	return details;
+    	}
+    	
+    	return null;
+    }
+    
+    private static class MongoConnectionDetails{
+    	public String user, password, host, database; 
+    	public int port;
     }
 }
